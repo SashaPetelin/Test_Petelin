6dd452a19f6ea36cdc0baa3b743c3e1ef9b58fa0
Sasha Petelin <alexanderzr58@icloud.com>
2022-10-15 22:41:00 +0300

    added C# code to solve the task

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] elementary = new string[6] {"Moscow", "123", "geek", "A4", "o", "hashtag"};
+string[] finite = new string[elementary.Length];
+
+Console.WriteLine("Начальный массив");
+PrintArray(elementary);
+Createfinite(elementary, finite);
+Console.WriteLine();
+Console.WriteLine("Массив с элементами не длиннее трех символов");
+PrintArray(finite);
+
+void Createfinite(string[] array, string[] array1)
+{
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array1[n] = array[i];
+            n++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
